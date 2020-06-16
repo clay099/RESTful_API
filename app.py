bd3a91502f33e400148cfe08f4e19ef04f33324a
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-06-16 10:29:11 -0700

    working views for all cupcakes, single cupcake & creating cupcake

--- app.py
+++ app.py
@@ -1,1 +1,51 @@
 """Flask app for Cupcakes"""
+from flask import Flask, render_template, redirect, render_template, jsonify, request
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///cupcakes"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+app.config['SECRET_KEY'] = 'Secret'
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+
+
+@app.route('/api/cupcakes')
+def show_all_cupcakes():
+    """Returns JSON for all cupcakes in DB"""
+
+    all_cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=all_cupcakes)
+
+
+@app.route('/api/cupcakes/<int:cupcake_id>')
+def show_one_cupcake(cupcake_id):
+    """Returns JSON for searched cupcake"""
+
+    returned_cupcake = Cupcake.query.get_or_404(cupcake_id)
+    return jsonify(cupcake=returned_cupcake.serialize())
+
+
+@app.route('/api/cupcakes', methods=['POST'])
+def create_cupcake():
+    """
+    Create new Cupcake in DB 
+    Returns JSON of created cupcake
+    """
+    flavor = request.json['flavor']
+    size = request.json['size']
+    rating = request.json['rating']
+    image = request.json.get('image', None)
+
+    new_cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image=image)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    response_json = jsonify(cupcake=new_cupcake.serialize())
+    return (response_json, 201)
