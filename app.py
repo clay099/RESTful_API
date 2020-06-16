deb493e23fad50b1ea2bb4e55ac6df04023b25d6
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-06-16 11:36:21 -0700

    working patch & delete tests

--- app.py
+++ app.py
@@ -49,3 +49,28 @@ def create_cupcake():
 
     response_json = jsonify(cupcake=new_cupcake.serialize())
     return (response_json, 201)
+
+
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=['PATCH'])
+def update_cupcake(cupcake_id):
+    """
+    Updated cupcake from JSON
+    retuns updated cupcake JSON
+    """
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    cupcake.flavor = request.json.get('flavor', cupcake.flavor)
+    cupcake.size = request.json.get('size', cupcake.size)
+    cupcake.rating = request.json.get('rating', cupcake.rating)
+    cupcake.image = request.json.get('image', cupcake.image)
+    db.session.add(cupcake)
+
+    return jsonify(cupcake=cupcake.serialize())
+
+
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=['DELETE'])
+def delete_cupcake(cupcake_id):
+    found_cupcake = Cupcake.query.get_or_404(cupcake_id)
+    db.session.delete(found_cupcake)
+    db.session.commit()
+
+    return jsonify(message="Deleted")
--- tests.py
+++ tests.py
@@ -107,3 +107,27 @@ class CupcakeViewsTestCase(TestCase):
             })
 
             self.assertEqual(Cupcake.query.count(), 2)
+
+    def test_update_cupcake(self):
+        with app.test_client() as client:
+            url = f"/api/cupcakes/{self.cupcake.id}"
+            resp = client.patch(url, json={"flavor": "UpdatedTestFlavor"})
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(resp.json, {
+                "cupcake": {
+                    "id": self.cupcake.id,
+                    "flavor": "UpdatedTestFlavor",
+                    "size": "TestSize",
+                    "rating": 5,
+                    "image": "http://test.com/cupcake.jpg"
+                }
+            })
+
+    def test_delete_cupcake(self):
+        with app.test_client() as client:
+            url = url = f"/api/cupcakes/{self.cupcake.id}"
+            resp = client.delete(url)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json, {"message": "Deleted"})
